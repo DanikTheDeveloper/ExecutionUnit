8065c0f230e683449d62215ebe4ccb0f91a40002
DanikTheDeveloper <69887442+DanikTheDeveloper@users.noreply.github.com>
2024-12-04 21:19:18 -0800

    32

--- SourceCode/sra_222222_32.vhd
+++ SourceCode/sra_222222_32.vhd
@@ -2,19 +2,21 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity sll_222222_32 is
+entity sra_222222_32 is
     Generic ( N : natural := 64 );
     Port (
         A     : in  std_logic_vector(N-1 downto 0);
         B     : in  std_logic_vector(6 downto 0);
         Result: out std_logic_vector(N-1 downto 0)
     );
-end entity sll_222222_32;
+end entity sra_222222_32;
 
-architecture behavioral of sll_222222_32 is
+architecture behavioral of sra_222222_32 is
     signal zeros : std_logic_vector(N-1 downto 0);
+    signal ones : std_logic_vector(N-1 downto 0);
 begin
     zeros <= (others => '0');
+	 ones <= (others => '1');
 
     process(A)
         variable temp : std_logic_vector(N-1 downto 0);
@@ -23,13 +25,14 @@ begin
         temp := A;
         
         for i in 5 downto 0 loop
-            if B(i) = '1' then
-                -- Shift left: append zeros based on current bit position
-						temp := temp(N-1-i downto 0) & zeros(N-1 downto N-i);
-            end if;
+					 if B(i) = '1' and A(N-1) = '0' then
+						temp := zeros(N-1 downto N-i) & temp(N-1 downto i);
+					elsif B(i) = '1' and A(N-1) = '1' then
+						temp := ones(N-1 downto N-i) & temp(N-1 downto i);
+					end if;
         end loop;
         
         -- After the loop, assign temp to Atemp signal
-        Result <= zeros(31 downto 0) & temp(31 downto 0);
+        Result <= temp;
     end process;
 end architecture behavioral;
