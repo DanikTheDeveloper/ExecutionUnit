4eaeada7007483ebc5a18cbd94391ff54a4a997c
DanikTheDeveloper <69887442+DanikTheDeveloper@users.noreply.github.com>
2024-12-05 20:30:20 -0800

    shiftFix

--- SourceCode/Mask.vhd
+++ SourceCode/Mask.vhd
@@ -14,9 +14,5 @@ architecture Behaviour of Mask is
     signal updA : std_logic_vector(N-1 downto 0);
 
 begin
-B(5 downto 0) <= A(5 downto 0);
-updA(N-8 downto 0) <= A(N-1 downto 7);
-updA(N-1 downto N-7) <= (others => '0');
-i2: ENTITY WORK.GiantOrGate generic map ( N => N )
-						port map( A => updA , B => B(6));
+B <= A(6 downto 0);
 end architecture Behaviour;
